0cb87becf971947450e217e8598b1198f34f705e
Erick Floresca <160937182+erick-floresca-nttd@users.noreply.github.com>
2025-02-03 18:41:40 -0500

    add unit test

--- tests/post_deploy_functional_readonly/main_test.go
+++ tests/post_deploy_functional_readonly/main_test.go
@@ -33,5 +33,5 @@ func TestAppConfigurationModule(t *testing.T) {
 		SetTestConfigFileName(infraTFVarFileNameDefault).
 		Build()
 
-	lib.RunSetupTestTeardown(t, *ctx, testimpl.TestComposableComplete)
+	lib.RunSetupTestTeardown(t, *ctx, testimpl.TestAppConfiguration)
 }
--- tests/testimpl/test_impl.go
+++ tests/testimpl/test_impl.go
@@ -1,16 +1,61 @@
 package testimpl
 
 import (
+	"os"
 	"testing"
+	"context"
 
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/cloud"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	// "github.com/Azure/azure-sdk-for-go/sdk/data/azappconfig"
+	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/launchbynttdata/lcaf-component-terratest/types"
 	"github.com/stretchr/testify/assert"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/appconfiguration/armappconfiguration/v2"
 )
 
-func TestComposableComplete(t *testing.T, ctx types.TestContext) {
-	// TODO: Remove this test from your module once you have defined some other tests.
-	t.Run("TestAlwaysSucceeds", func(t *testing.T) {
-		assert.Equal(t, "foo", "foo", "Should always be the same!")
-		assert.NotEqual(t, "foo", "bar", "Should never be the same!")
+func TestAppConfiguration(t *testing.T, ctx types.TestContext) {
+	subscriptionId := os.Getenv("ARM_SUBSCRIPTION_ID")
+
+	if len(subscriptionId) == 0 {
+		t.Fatal("ARM_SUBSCRIPTION_ID environment variable is not set")
+	}
+
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		t.Fatalf("Unable to get credentials: %e\n", err)
+	}
+
+	options := arm.ClientOptions{
+		ClientOptions: azcore.ClientOptions{
+			Cloud: cloud.AzurePublic,
+		},
+	}
+
+	clientFactory, err := armappconfiguration.NewClientFactory(subscriptionId, cred, &options)
+	if err != nil {
+		t.Fatalf("failed to create client: %v", err)
+	}
+
+	t.Run("TestAppConfiguration", func(t *testing.T) {
+		// resourceGroupName := terraform.Output(t, ctx.TerratestTerraformOptions(), "resource_group_name")
+		appconfigName := terraform.Output(t, ctx.TerratestTerraformOptions(), "app_configuration_name")
+
+		resp, err := clientFactory.NewOperationsClient().CheckNameAvailability(context.Background(), armappconfiguration.CheckNameAvailabilityParameters{
+			Name: to.Ptr(appconfigName),
+			Type: to.Ptr(armappconfiguration.ConfigurationResourceTypeMicrosoftAppConfigurationConfigurationStores),
+		}, nil)
+		if err != nil {
+			t.Fatalf("failed to finish the request: %v", err)
+		}
+
+		assert.True(t, *resp.NameAvailable)
 	})
+
+	// TODO
+	// t.Run("TestKeyValues", func(t *testing.T) {
+	// }
 }
